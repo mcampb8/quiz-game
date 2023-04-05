deb82490fb862876ce68e5e09f7dd594939bdc61
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-04 23:55:43 -0700

    final edits

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var button = document.createElement("button");
 button.innerText = "Start button"
 document.body.appendChild(button);
-var secondsRemaining = 100;
 var time = document.getElementById("time");
 console.log(time);
 var showButtons = document.getElementById("buttons");
 var askQuestion
 var playerAnswer
 var currentQuestionIndex =0;
-
+var score =0;
+var secondsRemaining = 100;
+//array of data
 var questions = [{
     query: "Who won the World Cup in 2022?",
     possibleAnswers: ["France", "Argentina", "Brazil", "England"],
@@ -33,17 +34,20 @@ var questions = [{
     possibleAnswers: ["Liverpool", "Bayern Munich", "Manchester City", "Real Madrid"],
     correctAnswer: "Real Madrid"
 }];
+var questionsRemaining = questions.length;
 
 function startTimer(){
     var timerInterval = setInterval(function() {
         secondsRemaining--;
         time.textContent = secondsRemaining + " seconds remaining"
-        if(secondsRemaining===0){
+        if(secondsRemaining===0 || questionsRemaining === 0){
             clearInterval(timerInterval);
+            displayScore();
         }
 
     }, 1000);
 }
+// start timer on button click
 button.addEventListener("click", function(){
     startTimer();
     displayQuestion();
@@ -54,25 +58,39 @@ function displayQuestion(){
     var dispAnswers = questions[currentQuestionIndex].possibleAnswers;
     var showQuestion = questions[currentQuestionIndex].query;
     console.log(showQuestion);
-    for(var j = 0; j<dispAnswers.length; j++){
-        var singleAnswers = dispAnswers[j];
+    for(var i = 0; i<dispAnswers.length; i++){
+        var singleAnswers = dispAnswers[i];
         var answerButton = document.createElement("button");
         answerButton.innerText = singleAnswers;
+        answerButton.addEventListener("click", checkAnswer);
         document.getElementById("buttons").appendChild(answerButton);
-        answerButton.addEventListener("click", checkAnswer)
-
     }
     document.getElementById("question").textContent = showQuestion;
  }
+ //function to display the score
+ function displayScore(){
+    console.log(score); 
+    currentQuestionIndex = 0;
+    secondsRemaining =100;
+    localStorage.setItem("score:", score);
+ }
+ // function to check the answer
  function checkAnswer(event){
+    questionsRemaining--;
+    console.log(questionsRemaining);
     var answerText = event.target.textContent;
      if(answerText !== questions[currentQuestionIndex].correctAnswer){
     secondsRemaining = secondsRemaining - 10;
 } else{
-    console.log("correct");
+    score++;
 }
     currentQuestionIndex++;
+    if(questionsRemaining !== 0){
     displayQuestion();
+    }else{
+        displayScore();
+    }
+   
  }
 
 
